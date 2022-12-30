2aa5eddabc5b194ea469658277b72f3f06681032
Riyaldi Hasan <riy.2923@gmail.com>
2022-12-30 11:00:44 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/riyaldi/tempat_wisata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.riyaldi.tempat_wisata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:draggable_home/draggable_home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+    title: "Draggable Home",
+    home: HomePage(),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableHome(
+      leading: const Icon(Icons.arrow_back_ios),
+      title: const Text("Draggable Home"),
+      actions: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+      ],
+      headerWidget: headerWidget(context),
+      headerBottomBar: headerBottomBarWidget(),
+      body: [
+        listView(),
+      ],
+      fullyStretchable: false,
+      backgroundColor: Colors.white,
+      appBarColor: Colors.teal,
+    );
+  }
+
+  Row headerBottomBarWidget() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Icon(
+          Icons.settings,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+
+  Widget headerWidget(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.blueAccent,
+        image: DecorationImage(
+            image: AssetImage("assets/images/cloudySky.jpg"),
+            fit: BoxFit.cover),
+      ),
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Text(
+          "Ojo Lali Hiling",
+          style: Theme.of(context)
+              .textTheme
+              .headline4!
+              .copyWith(color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  ListView listView() {
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 0),
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 20,
+      shrinkWrap: true,
+      itemBuilder: (context, index) => Card(
+        color: Colors.white70,
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Text("$index"),
+          ),
+          title: const Text("Title"),
+          subtitle: const Text("Subtitile"),
+        ),
+      ),
+    );
+  }
+}
